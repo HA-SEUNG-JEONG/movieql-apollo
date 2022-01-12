69a72367104eb06eabb74ff7e84d67ba9fa14a31
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-01-12 11:09:35 +0900

    Local State Part One - resolver, @client

--- src/Apollo.js
+++ src/Apollo.js
@@ -3,6 +3,11 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
   uri: "https://movieql2.vercel.app/ ",
   // uri: " https://movieql2.vercel.app/ ",
+  resolvers: {
+    Movie: {
+      isLiked: () => false,
+    },
+  },
   cache: new InMemoryCache(),
 });
 
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div`
-  height: 380px;
+  height: 400px;
+  border-radius: 7px;
   width: 100%;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-  overflow: hidden;
-  border-radius: 7px;
+  background-color: transparent;
 `;
 
 const Poster = styled.div`
@@ -18,10 +18,11 @@ const Poster = styled.div`
   background-position: center center;
 `;
 
-export default ({ id, bg }) => (
+export default ({ id, bg, isLiked }) => (
   <Container>
     <Link to={`/${id}`}>
       <Poster bg={bg} />
     </Link>
+    <button>{isLiked ? "Unlike" : "Like"}</button>
   </Container>
 );
